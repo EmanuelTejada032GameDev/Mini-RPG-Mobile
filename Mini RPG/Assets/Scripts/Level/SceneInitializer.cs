a2e8c9f79dde18db32d90b3a0d586243027fc2fc
EmanuelTejada032GameDev <emanueltejada311295@gmail.com>
2022-07-18 23:49:49 -0400

    Scripttable Object setup
    Import scriptable object asset from unity asset store,
    Add different assembly definitions for each script directory structure
    Add some scriptable object startting setup for scene management

---
+++ Mini RPG/Assets/Scripts/Level/SceneInitializer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class SceneInitializer : MonoBehaviour
+{
+    [Header("Dependencies")]
+    public SceneSO[] sceneDependencies;
+
+    [Header("On scene readey")]
+    public UnityEvent onDependenciesLoaded;
+
+    private void Start()
+    {
+        StartCoroutine(LoadDependencies());
+    }
+
+    private IEnumerator LoadDependencies()
+    {
+        for (int i = 0; i < this.sceneDependencies.Length; i++)
+        {
+            SceneSO sceneToLoad = this.sceneDependencies[i];
+            if (SceneManager.GetSceneByName(sceneToLoad.name).isLoaded == false)
+            {
+                var loadOperation = SceneManager.LoadSceneAsync(sceneToLoad.name, LoadSceneMode.Additive);
+                while (loadOperation.isDone == false)
+                {
+                    yield return null;
+                }
+            }
+        }
+        if(onDependenciesLoaded != null)
+            onDependenciesLoaded.Invoke();
+    }
+
+}
---
+++ Mini RPG/Assets/Scripts/ScriptableObjects/LevelEntranceSO.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewLevel", menuName = "Scriptable Objects/Levels/Level Entrance")]
+public class LevelEntranceSO : ScriptableObject
+{
+}
